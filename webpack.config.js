9587ec35138922931caba75e3f06d51786ffb100
lilei <1052048489@qq.com>
2018-06-23 14:47:05 +0800

    add
    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./index.js",
+    entry: {
+        app:"./index.js"
+    },
     output: {
         path: __dirname+"/dist",
         filename: "bundle.js"
     },
     module: {
         
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            chunks:['app'],
+            filename:'index.html',
+            template:'html/testModule.html'
+        })
+    ],
+    mode:"development"
 };

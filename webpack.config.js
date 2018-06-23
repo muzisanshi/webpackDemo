ac553f846837fa4c8f0a6132acd4ff07c3edded5
lilei <1052048489@qq.com>
2018-06-23 16:08:51 +0800

    add
    add

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        
+        rules: [
+          { test: /\.scss$/, use: ['style-loader','css-loader','sass-loader']}
+        ]
     },
     plugins:[
         new HtmlWebpackPlugin({
